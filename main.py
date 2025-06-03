58c2cd7c7e47d9feac1d93f97bca27fa3bbab8a6
Raltor123 <cornijon@gmail.com>
2025-06-03 14:59:11 +0200

    feat: qft done but not tested

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from mpqp.gates import *
 from mpqp.measures import BasisMeasure
 from mpqp.execution import run, AWSDevice
 
-def basicCircuit():
-    circ = QCircuit(2)
-    circ.add([H(0), CNOT(0, 1), Barrier(), BasisMeasure(shots=1000)])
-    circ.pretty_print()
+from quantum.qft import AddQFTToCircuit
 
-    print(run(circ, [AWSDevice.BRAKET_LOCAL_SIMULATOR]))
+def basicCircuit():
+    circ = QCircuit(3)
+
+    AddQFTToCircuit(circ)
+
+    circ.pretty_print()
+    #print(run(circ, [AWSDevice.BRAKET_LOCAL_SIMULATOR]))
 
 if __name__ == "__main__":
     basicCircuit()
--- quantum/qft.py
+++ quantum/qft.py
@@ -1,8 +1,18 @@
 from mpqp import QCircuit
+from mpqp.gates import *
+
 
 """
 This function add a QFT at the end of the circuit
 """
 def AddQFTToCircuit(circ: QCircuit) -> None:
-    # TODO
-    print(circ)
+    nbQubits : int = circ.nb_qubits
+    
+    for i in range(nbQubits):
+        circ.add(H(i))
+
+        for j in range(i + 1, nbQubits):
+            circ.add(CRk(j-i+1, j, i))
+    
+    for i in range((nbQubits // 2)):
+        SWAP(i, nbQubits - i - 1)

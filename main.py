727ddcb3bfd36144d32afa197a3fa973e72b0067
Raltor123 <cornijon@gmail.com>
2025-06-05 19:24:21 +0200

    Made Shor's function to compute everything

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from mpqp import QCircuit, Barrier
-from mpqp.gates import *
-from mpqp.measures import BasisMeasure
-from mpqp.execution import run, AWSDevice
+from mpqp import QCircuit
 
-from quantum.qft import AddQFTToCircuit
-from quantum.quantumSubroutine import ComputePeriod
+from classic.preprocess import FindCoPrime
+from classic.postprocess import PostProcessPeriod
 
-def basicCircuit():
-    circ = QCircuit(5)
+from quantum.quantumSubroutine import ComputePeriods, QuantumSubroutine
 
-    AddQFTToCircuit(circ, 3)
-    circ.pretty_print()
+def Shor(N: int) -> tuple[int, int | str]:
+    f1 = None # Answer, loop until find
+    tested_a = []
+    a = None
+    while f1 == None:
+        # Finding an a not already tested
+        a = FindCoPrime(N)
+        while a in tested_a:
+            a = FindCoPrime(N)
+        tested_a.append(a)
 
-    circ.add(BasisMeasure(shots=100))
-    print(f"Period is {ComputePeriod(circ)}")
+        # Create the circuit
+        circ = QuantumSubroutine(N, a)
+
+        # Compute periods
+        periods = ComputePeriods(circ)
+
+        # Test all periods
+        for r in periods: 
+            # Compute primes
+            f1, f2 = PostProcessPeriod(N, a, r)
+            
+            if f1 != None: # Primes are founded
+                return f1, f2
+        
+        # If every a has been tested, end the loop
+        # a is include in [2, N[, which is N-3 elements
+        if len(tested_a) == N - 3:
+            return None, "Not found"
+
 
 if __name__ == "__main__":
-    basicCircuit()
+    print(Shor(15))

c15681499ab8b18a97adc387ea72b156d90d7714
Raltor123 <cornijon@gmail.com>
2025-06-05 19:23:47 +0200

    qSubroutine changed compute periods to return a list instead of just most probable r, and added some structure in QuantumSubroutine function

--- quantum/quantumSubroutine.py
+++ quantum/quantumSubroutine.py
@@ -1,8 +1,13 @@
 # choose t number of qubits in the phase register
+import numpy as np
+
 from mpqp import QCircuit
 from mpqp.gates import *
 from mpqp.execution import run, AWSDevice
-from classic.preprocess import FindCoPrime, PrecomputePowers, FindPhaseRegisterSize, FindModularRegisterSize
+from mpqp.measures import BasisMeasure
+
+from classic.preprocess import PrecomputePowers, FindPhaseRegisterSize, FindModularRegisterSize
+from quantum.qft import AddQFTToCircuit
 
 def QuantumModularExponentiation(Circ, a: int, n: int, N: int) -> None:
     t = len(Circ)
@@ -12,30 +17,38 @@ def QuantumModularExponentiation(Circ, a: int, n: int, N: int) -> None:
             Circ.add(C[i % t], i + t)
 
 
-def QuantumSubroutine(a: int, N: int) -> QCircuit:
-
+def QuantumSubroutine(N: int, a: int) -> QCircuit:
     t = FindPhaseRegisterSize(N)
     n = FindModularRegisterSize(N)
     # prepare two register: |0> \tensor t \tensor |0> \tensor n
-    Circ = QCircuit(t + n)
+    circ = QCircuit(t + n)
 
     # Apply Hadamard on each qubit of register 1
     for i in range(t):
-        Circ.add(H(i))
+        circ.add(H(i))
 
-    QuantumModularExponentiation(Circ, a, n, N)
+    # TODO: Do the circuit
+    #QuantumModularExponentiation(circ, a, n, N)
+
+    AddQFTToCircuit(circ, t)
+
+    circ.add(BasisMeasure(list(range(t)), shots=100))
+    return circ
 
 
 
 """
 This function takes a quantum circuit (generated from QuantumSubroutine),
-emulates it and return the result (most probable period r).
+emulates it and return the result (most probable periods (above a threshold)).
 """
-def ComputePeriod(circ: QCircuit) -> int:
+def ComputePeriods(circ: QCircuit) -> int:
     result = run(circ, AWSDevice.BRAKET_LOCAL_SIMULATOR)
 
-    r = 0
-    for i in range(1, len(result.counts)):
-        r = i if result.counts[i] > result.counts[r] else r
-    
-    return r
+    # Getting only non-0 results
+    results = [(i, result.probabilities[i]) for i in range(len(result.probabilities)) if result.probabilities[i] != 0]
+
+    # Compute threshold
+    threshold = np.percentile([results[i][1] for i in range(len(results))], 90)
+
+    # Return only 10% of most probable answers
+    return [results[i][0] for i in range(len(results)) if results[i][1] >= threshold]

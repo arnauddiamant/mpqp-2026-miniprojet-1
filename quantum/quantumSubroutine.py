c778702ed78a2273a536e0538cb0c34d154acb60
thomas.florion <thomas.florion@epita.fr>
2025-06-09 23:49:06 +0200

    feat: it maybe good, it maybe not

--- quantum/quantumSubroutine.py
+++ quantum/quantumSubroutine.py
@@ -1,6 +1,7 @@
 # choose t number of qubits in the phase register
 from mpqp import QCircuit
 from mpqp.gates import *
+from mpqp.measure import BasisMeasure
 from classic.preprocess import FindCoPrime, PrecomputePowers, FindPhaseRegisterSize, FindModularRegisterSize
 
 def QuantumModularExponentiation(Circ, a: int, n: int, N: int) -> None:
@@ -9,6 +10,15 @@ def QuantumModularExponentiation(Circ, a: int, n: int, N: int) -> None:
     for i in range(t + n - 1):
         if Circ[i] == 1:
             Circ.add(C[i % t], i + t)
+
+def InverseQFT(Circ, start: int, end: int) -> None:
+    for i in range(end - 1, start - 1, -1):
+        Circ.add(H(i))
+        for j in range(i - 1, start - 1, -1):
+            Circ.add(CRk(-2 ** (i - j), j, i))
+
+    for i in range((end - start) // 2):
+        Circ.add(SWAP(start + i, end - i - 1))
 
 
 def QuantumSubroutine(a: int, N: int) -> QCircuit:
@@ -24,4 +34,10 @@ def QuantumSubroutine(a: int, N: int) -> QCircuit:
 
     QuantumModularExponentiation(Circ, a, n, N)
 
+        # Apply inverse QFT to phase register
+    InverseQFT(Circ, 0, t)
 
+    # Measure the phase register
+    Circ.add(BasisMeasure(targets=list(range(t)), shots=1024))
+
+    return Circ

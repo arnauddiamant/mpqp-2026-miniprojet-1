9bb08f1fb7948d1a86420bb97c88274a19cdc409
Raltor123 <cornijon@gmail.com>
2025-06-12 14:44:26 +0200

    some debug

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from classic.postprocess import PostProcessPeriod
 from quantum.quantumSubroutine import ComputePeriods, QuantumSubroutine
 
 def Shor(N: int, maxIterations: int = -1, display_circ: bool = False, DEBUG_MODE: bool = False) -> tuple[int, int | str]:
-    maxIterations = N if maxIterations != -1 else N // 2
+    maxIterations = maxIterations if maxIterations != -1 else N // 2 - 1
     f1 = None # Answer, loop until founded
     tested_a = []
     a = None
     if DEBUG_MODE:
-        print(f"\n=== Starting Shor's algorithm for N = {N} ===")
+        print(f"\n\033[1m=== Starting Shor's algorithm for N = {N} ===\033[0m")
     while f1 == None:
         # Finding an a not already tested
         a = FindCoPrime(N)
         while a in tested_a:
             a = FindCoPrime(N)
         if DEBUG_MODE:
-            print(f"\nTesting a = {a} (already tested: {tested_a})")
+            print(f"\n\nTesting a = {a} (already tested: {tested_a})")
         tested_a.append(a)
 
         # Create the circuit
@@ -70,11 +70,15 @@ if __name__ == "__main__":
     # Select a number
     N = int(input("Choose a number to factorize: "))
 
+    if N < 4:
+        print("Invalid output. N must be >= 4.")
+        exit(1)
+
     # If number is even, exit
     if not (N & 1):
         if DEBUG_MODE:
             print("N is even. Computing solution.")
-        print(f"{N} = 2 * {N >> 1}")
+        print(f"\n\033[92mEven number: {N} = 2 * {N >> 1}\033[0m")
         exit()
 
     if DEBUG_MODE:
@@ -83,6 +87,6 @@ if __name__ == "__main__":
     # Use the shor's algorithm to factorize N
     f1, f2 = Shor(N, display_circ=args.circuit, DEBUG_MODE=DEBUG_MODE)
     if f1 == None:
-        print("No factorization founded.")
+        print("\n\033[91mNo factorization founded.\033[0m")
     else:
-        print(f"\nResult: {N} = {f1} * {f2}")
+        print(f"\n\033[92mResult: {N} = {f1} * {f2}\033[0m")
